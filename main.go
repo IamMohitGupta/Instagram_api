d15312f9021102c99c3e6cad3591888a2e72fdf1
IamMohitGupta <72707911+IamMohitGupta@users.noreply.github.com>
2021-10-09 22:12:46 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -7,18 +7,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 type User struct {
-	Id       int    `json:"User_id"`
+	Id       string `json:"User_id"`
 	Name     string `json:"User_name"`
 	Email    string `json:"User_email"`
 	Password string `json:"Password"`
 }
 type Post struct {
-	Id               int    `json:"Post_id"`
+	Id               string `json:"Post_id"`
 	Caption          string `json:"Post_caption"`
 	Image_url        string `json:"Post_url"`
 	Posted_Timestamp string `json:"Post_time"`
@@ -60,8 +58,8 @@ type Users []User
 
 func allUsers(w http.ResponseWriter, r *http.Request) {
 	Users := Users{
-		User{Id: 1, Name: "James", Email: "mgupta060402@gmail.com", Password: "Hello"},
-		User{Id: 2, Name: "James", Email: "mgupta060402@gmail.com", Password: "Hello"},
+		User{Id: "User#1", Name: "James", Email: "mgupta060402@gmail.com", Password: "Hello"},
+		User{Id: "User#2", Name: "James", Email: "mgupta060402@gmail.com", Password: "Hello"},
 	}
 	fmt.Print("All the users are listed")
 	json.NewEncoder(w).Encode(Users)
@@ -71,7 +69,7 @@ type Posts []Post
 
 func allPosts(w http.ResponseWriter, r *http.Request) {
 	Posts := Posts{
-		Post{Id: 1, Caption: "First Post", Image_url: "http://hello.jpg", Posted_Timestamp: "12:00:00 UTC"},
+		Post{Id: "Post_1", Caption: "First Post", Image_url: "http://hello.jpg", Posted_Timestamp: "12:00:00 UTC"},
 	}
 	fmt.Print("All the posts are listed")
 	json.NewEncoder(w).Encode(Posts)
@@ -80,60 +78,14 @@ func allPosts(w http.ResponseWriter, r *http.Request) {
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the homepageeeee!")
 }
-
-// func handleRequests() {
-// 	http.HandleFunc("/hello", homePage)
-// 	http.HandleFunc("/users", allUsers)
-// 	http.HandleFunc("/posts", allPosts)
-// 	log.Fatal(http.ListenAndServe(":8000", nil))
-// }
-// func createEvent(w http.ResponseWriter, r *http.Request) {
-// 	var newEvent event
-// 	reqBody, err := ioutil.ReadAll(r.Body)
-// 	if err != nil {
-// 		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-// 	}
-
-// 	json.Unmarshal(reqBody, &newEvent)
-// 	events = append(events, newEvent)
-// 	w.WriteHeader(http.StatusCreated)
-
-// 	json.NewEncoder(w).Encode(newEvent)
-// }
-// func getOneEvent(w http.ResponseWriter, r *http.Request) {
-// 	eventID := mux.Vars(r)["id"]
-
-// 	for _, singleEvent := range events {
-// 		if singleEvent.ID == eventID {
-// 			json.NewEncoder(w).Encode(singleEvent)
-// 		}
-// 	}
-// }
-
-// func main() {
-
-// 	handleRequests()
-
-// }
-func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homePage)
-	router.HandleFunc("/users", allUsers)
-	router.HandleFunc("/posts", allPosts)
-	log.Fatal(http.ListenAndServe(":8000", router))
+func handleRequests() {
+	http.HandleFunc("/hello", homePage)
+	http.HandleFunc("/users", allUsers)
+	http.HandleFunc("/posts", allPosts)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
+func main() {
 
-// http.Get("/foo", homeLink)
+	handleRequests()
 
-// http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-// })
-
-// log.Fatal(http.ListenAndServe(":8080", nil))
-
-// resp, err := http.Get("http://example.com/")
-// if err != nil {
-// 	// handle error
-// }
-// defer resp.Body.Close()
-// body, err := io.ReadAll(resp.Body)
+}
